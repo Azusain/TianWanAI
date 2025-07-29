04bb64a324fed870c64b0e00756efbc1dea2f475
Azusain <azusaings@gmail.com>
2025-07-29 19:02:01 +0800

    chore: gesture service

---
+++ api.py
@@ -0,0 +1,103 @@
+import os
+import re
+
+# api
+from enum import Enum
+from uuid import uuid4 as uuid4
+
+# model
+from ultralytics import YOLO
+import torch
+
+# logger
+from loguru import logger
+
+VERSION_API = '0.0.1'
+
+class ServiceStatus(Enum):
+    SUCCESS = 0
+    INVALID_CONTENT_TYPE = -1
+    MISSING_IMAGE_DATA = -2
+    INVALID_IMAGE_FORMAT = -3
+    NO_OBJECT_DETECTED = -4
+    
+    def stringify(err_no):
+        if err_no == ServiceStatus.SUCCESS.value:
+            return 'SUCCESS'
+        elif err_no == ServiceStatus.INVALID_CONTENT_TYPE.value:
+            return'INVALID_CONTENT_TYPE'
+        elif err_no == ServiceStatus.MISSING_IMAGE_DATA.value: 
+            return 'MISSING_IMAGE_DATA'    
+        elif err_no == ServiceStatus.INVALID_IMAGE_FORMAT.value:
+            return'INVALID_IMAGE_FORMAT'
+        elif err_no == ServiceStatus.NO_OBJECT_DETECTED.value:
+            return 'NO_OBJECT_DETECTED'
+
+class GestureService():
+    def __init__(self, model_path, imgsz) -> None:
+        self.version = "0.0.1"
+        self.path = model_path
+        self.imgsz = imgsz
+        
+        if not os.path.exists(model_path):
+            logger.error(f"model path doesn't exist: {model_path}")
+            exit(1)
+        
+        # loading model
+        logger.debug("loading model...")
+        self.model = YOLO(self.path)
+        self.device = (
+            torch.device('cuda') 
+            if torch.cuda.is_available() 
+            else torch.device('cpu')
+        )
+        self.model.to(self.device)
+        self.is_half = (self.device == 'cpu')
+        
+        # model pre-heating
+        logger.debug("model preheating...")
+        rand_data = torch.randn(1, 3, 640, 640)
+        rand_data = (rand_data - torch.min(rand_data)) / (torch.max(rand_data) - torch.min(rand_data))
+        self.model.predict(
+            source=rand_data,
+            verbose=False
+        )
+        
+    def Predict(self, img):
+        return self.model.predict(
+            source=img, 
+            imgsz=self.imgsz,
+            half=self.is_half,
+            verbose=False
+        )
+
+    def Response(self, err_no, label=None, score=None, xyxyn=None):
+        err_msg = ServiceStatus.stringify(err_no)
+        if err_no != 0:
+            logger.error(err_msg)
+        else:
+            logger.success(err_msg + f" - open: {score}")
+        
+        left, top, width, height = None, None, None, None
+        if score is not None and xyxyn is not None:
+            xyxyn = xyxyn[0].tolist() 
+            left    = xyxyn[0]
+            top     = xyxyn[1]
+            width   = xyxyn[2] - xyxyn[0]
+            height  = xyxyn[3] - xyxyn[1]
+        return {
+            "log_id": uuid4(),
+            "err_no": err_no,
+            "err_msg": err_msg,
+            "api_version": VERSION_API,
+            "model_version": self.version,
+            "results": [{
+                "score": score,
+                "location": {
+                    "left": left,
+                    "top": top,
+                    "width": width,
+                    "height": height
+                }
+            }]
+        }
